6bddde205e77e9515cb1d5950b7ca5d80ec2efe2
Nick Marucci <npm1514@gmail.com>
2016-05-29 12:14:53 -0600

    day 9 code complete

---
+++ Day9/code/controllers/dogCtrl.js
@@ -0,0 +1,44 @@
+var mongojs = require('mongojs'); //call in mongojs to connect to mongo database
+var ObjectId = require('mongodb').ObjectId; //why is this necessary...something to do with id for put and deletes
+
+var dbdog = mongojs('dogs', ['doginfo']); //setupp mongo structure
+
+module.exports = { //end point descriptions
+  create: function(req, res, next) {
+    dbdog.doginfo.insert(req.body, function(err, result){ //call into database in the doginfo collection and insert new data
+      if (err) {
+        res.send(err);
+      }
+      res.send(result);
+    });
+  },
+  read: function(req, res, next) {
+    dbdog.doginfo.find({}, function(err, result){ //call into database in the doginfo collection and retrieve all data
+      if (err) {
+        res.send(err);
+      }
+      res.send(result);
+    });
+  },
+  update: function(req, res, next){
+    var updateObj = { //setup object for modification
+      query: {_id: ObjectId(req.params.id)},
+      update: {$set: req.body},
+      new: false
+    };
+    dbdog.doginfo.findAndModify(updateObj, function(err, result){ //call into database in the doginfo collection and based on id and new data change existing data
+      if (err) {
+        res.send(err);
+      }
+      res.send(result);
+    });
+  },
+  delete: function(req, res, next){
+    dbdog.doginfo.remove({_id:ObjectId(req.params.id)}, function(err, result){ //call into database in the doginfo collection and based on id, remove selected data
+      if (err) {
+        res.send(err);
+      }
+      res.send("Successfully deleted");
+    });
+  }
+};
---
+++ Day9/code/index.js
@@ -0,0 +1,29 @@
+var express = require('express'); //makes coding node way nicer
+var bodyParser = require('body-parser'); //parses objects into json format
+var cors = require('cors'); //takes care of headers
+
+var dogCtrl = require('./controllers/dogCtrl'); //calling in controller documents
+var toyCtrl = require('./controllers/toyCtrl');
+
+var app = express(); //run express
+
+app.use(cors()); //run cors
+app.use(bodyParser.json()); //json-ifys req.body
+app.use(bodyParser.urlencoded()); //This made my code work why?????
+app.use(express.static(__dirname + '/public')); //automatically opens public folder and runs front end index.html when nodemon runs
+
+//end points
+app.post('/dogs', dogCtrl.create);
+app.get('/dogs', dogCtrl.read);
+app.put('/dogs/:id', dogCtrl.update);
+app.delete('/dogs/:id', dogCtrl.delete);
+
+app.post('/toys', toyCtrl.create);
+app.get('/toys', toyCtrl.read);
+app.put('/toys/:id', toyCtrl.update);
+app.delete('/toys/:id', toyCtrl.delete);
+
+//localhost:8000
+app.listen(8000, function(){
+  console.log("listening to 8000");
+});
