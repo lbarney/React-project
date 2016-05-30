b0225274c8a67f45b0f13581d1fd2926ece510d4
Nick Marucci <npm1514@gmail.com>
2016-05-30 11:57:01 -0600

    add day 6 code

--- Day06/code/app.js
+++ Day06/code/app.js
@@ -1,3 +1,85 @@
+var cars = [{
+  make: "Honda",
+  model: "CRV",
+  id: "123456"
+},
+{
+  make: "Honda",
+  model: "CRZ",
+  id: "456123"
+}];
+
+var newcar = car;
+newcar.model = "Civic";
+
+var id = car.id;
+
+function getcars() {
+  $.ajax({
+    method: "GET",
+    url: '/cars'
+  }).then(function(response){
+    //code here
+  });
+}
+
+function newCar(car) {
+  $.ajax({
+    method: 'POST',
+    data: car,
+    url: '/cars'
+  }).then(function(response){
+    getCars()
+  });
+}
+function changeCar(newcar) {
+  $.ajax({
+    method: "PUT",
+    url: '/cars/' + newcar.id,
+    data: newcar
+  }).then(function(response){
+    getCars();
+  });
+}
+
+function deleteCar(car) {
+  $.ajax({
+    method: "DELETE",
+    url: '/cars/' + car.id
+  }).then(function(response){
+    getCars();
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var cars;
+
 
 
 
@@ -5,7 +87,11 @@
       method: "GET",
       url: "/api"
     }).then(function(response){//setup asyncronous promise
-      console.log(response);//do something with response
+      if(response) {
+      console.log("get was successful");
+    } else {
+      console.log("error");
+    }
     });
 
     $.ajax({
